292207af0ff50974cb57f67264ba660293d11d89
Rémi AUGUSTE <remi.auguste@gmail.com>
2018-03-27 23:13:19 +0200

    poc pour nodejs et polymer

---
+++ bind.js
@@ -0,0 +1,32 @@
+/**
+ * Bind de façon à ce que la propriété destination soit synchro avec la propriété de la source
+ * @param {*} srcObj objet source à écouter
+ * @param {String} srcProp nom de la propriété à écouter
+ * @param {*} dstObj objet qui souhaite être synchro avec la source
+ * @param {String} dstProp nom de la propriété synchronisée
+ * @returns la source surchargée à utiliser
+ */
+function bind(srcObj, srcProp, dstObj, dstProp)
+{
+	return new Proxy(srcObj, {
+		set(obj, prop, valeur) {
+			if (prop == srcProp && dstObj[dstProp] != valeur)
+			{
+				dstObj[dstProp] = valeur;
+			}
+			obj[prop] = valeur;
+		}
+	});
+}
+
+/**
+ * Fonctionne dans le navigateur et dans NodeJS
+ */
+if (typeof window !== 'undefined')
+{
+	window.bind = bind;
+}
+else
+{
+	module.exports = bind;
+}
